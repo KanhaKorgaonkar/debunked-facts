b9bebca699abaf5f91dfe6ee29cd5e8ffed5fccb
Kanha Korgaonkar <kanhakorgaonkar@outlook.com>
2024-07-22 08:15:54 -0700

    modified:   src/app/utils/gemini.js

--- src/app/utils/gemini.js
+++ src/app/utils/gemini.js
@@ -23,7 +23,7 @@ function tryParseJSON(text) {
 export async function generateContent(year) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const prompt = `Provide a list of 5 scientific, historical, or widely accepted concepts that were considered factual or true in ${year} AND WERE TAUGHT IN SCHOOLS but have since been conclusively disproven or significantly revised. Return the results in the following JSON format:
+  const prompt = `Provide a list of 10 scientific, historical, or widely accepted concepts that were considered factual or true in ${year} AND WERE TAUGHT IN SCHOOLS but have since been conclusively disproven or significantly revised. Return the results in the following JSON format:
 
   {
     "results": [
@@ -37,7 +37,7 @@ export async function generateContent(year) {
     ]
   }
 
-  ONLY PRESENT 10 SPECIFIC FACTS WHICH HAVE been verifiable false now, but were taught in schools as true in that year, and may have now have changed our understanding across various fields, including natural sciences, medical sciences, social sciences, historical interpretations, and technological assumptions. Prioritize examples that have had significant impacts on their respective fields or on society at large. ONLY OUTPUT THE JSON DIRECTLY, DO NOT USE CODEBLOCK SYNTAX OR FORMATTING BEFORE.`;
+  ONLY PRESENT SPECIFIC FACTS WHICH HAVE been verifiable false now, but were taught in schools as true back then, and may have now have changed our understanding across various fields, including natural sciences, medical sciences, social sciences, historical interpretations, and technological assumptions. Prioritize examples that have had significant impacts on their respective fields or on society at large. ONLY OUTPUT THE JSON DIRECTLY, DO NOT USE CODEBLOCK SYNTAX OR FORMATTING BEFORE.`;
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
